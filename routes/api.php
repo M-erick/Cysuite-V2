cc332c9d363f9986107b49d99d5dccf915f41d24
M-erick <muriithierick3758@gmail.com>
2024-05-21 14:56:43 +0300

    updated the api endpoint and added the issue management functionality

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,12 @@
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoomController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\GuestController;
+use App\Http\Controllers\IssueController;
+use App\Http\Controllers\RatingController;
+use App\Http\Controllers\ResponseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -41,7 +45,50 @@ Route::post('/guests/register', [GuestController::class, 'register']);
 
 Route::post('/guests/login', [GuestController::class, 'login']);
 
+
+// Room routes
+
+Route::get('/rooms', [RoomController::class, 'index']);
+Route::post('/rooms', [RoomController::class, 'store']);
+Route::get('/rooms/{id}', [RoomController::class, 'show']);
+Route::put('/rooms/{id}', [RoomController::class, 'update']);
+Route::delete('/rooms/{id}', [RoomController::class, 'destroy']);
+
 Route::get('/room/{roomId}/details', function () {
     // Route accessible only to guests linked to the specified room: using the middleware check.guestAcess
 })->middleware('auth:guests', 'check.guestAcess');
 
+
+// Routes for  issues
+    // Route to list all issues
+    Route::get('/issues', [IssueController::class, 'index'])->name('issues.index');
+
+    // Route to create a new issue
+    Route::post('/issues', [IssueController::class, 'store'])->name('issues.store');
+
+    // Route to view a specific issue
+    Route::get('/issues/{id}', [IssueController::class, 'show'])->name('issues.show');
+
+    // Route to update an existing issue
+    Route::put('/issues/{id}', [IssueController::class, 'update'])->name('issues.update');
+
+    // Route to delete an issue
+    Route::delete('/issues/{id}', [IssueController::class, 'destroy'])->name('issues.destroy');
+
+// Routes for responses
+Route::post('/issues/{issue}/responses',[ResponseController::class,'store'])->name('responses.store');
+Route::get('/issues/{issue}/responses', [ResponseController::class,'index'])->name('responses.index');
+Route::get('/issues/{issue}/responses/{response}', [ResponseController::class,'show'])->name('responses.show');
+Route::put('/issues/{issue}/responses/{response}', [ResponseController::class,'update'])->name('responses.update');
+Route::delete('/issues/{issue}/responses/{response}', [ResponseController::class,'destroy'])->name('responses.destroy');
+
+
+//
+//Rating Page:will implement the  route authentication later
+// Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/ratings', [RatingController::class, 'index']);
+    Route::post('/ratings', [RatingController::class, 'store']);
+    Route::get('/ratings/{id}', [RatingController::class, 'show']);
+    Route::put('/ratings/{id}', [RatingController::class, 'update']);
+    Route::delete('/ratings/{id}', [RatingController::class, 'destroy']);
+// });
