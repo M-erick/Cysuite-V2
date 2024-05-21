9a0c0f72fb43b3bbb6372a9ef2a29ac2c3805e38
M-erick <muriithierick3758@gmail.com>
2024-05-21 14:39:10 +0300

    default response controller

---
+++ app/Http/Controllers/ResponseController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Response;
+use Illuminate\Http\Request;
+
+class ResponseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $responses = Response::all();
+        return response()->json($responses);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'response_text' => 'required|string',
+            'user_id' => 'required|integer',
+            'issue_id' => 'required|integer',
+        ]);
+
+        $response = Response::create($request->all());
+        return response()->json($response, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+        $response = Response::findOrFail($id);
+        return response()->json($response);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+        $request->validate([
+            'response_text' => 'required|string',
+            'user_id' => 'required|integer',
+            'issue_id' => 'required|integer',
+        ]);
+
+        $response = Response::findOrFail($id);
+        $response->update($request->all());
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+        $response = Response::findOrFail($id);
+        $response->delete();
+        return response()->json(null, 204);
+    }
+}
