12450cebab0baeeeaf46ba1a4f5cb18e343f4e1a
M-erick <muriithierick3758@gmail.com>
2024-05-21 05:07:41 +0300

    Completed the admin routes ,all that is remaining on this is security using sanctum or passport

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware(['auth:sanctum', 'check.supervisor'])->group(function () {
+    Route::post('/admins', [AdminController::class, 'store']);
+    Route::put('/admins/{id}', [AdminController::class, 'update']);
+    Route::delete('/admins/{id}', [AdminController::class, 'destroy']);
+// });
+
+// Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/admins', [AdminController::class, 'index']);
+    Route::get('/admins/{id}', [AdminController::class, 'show']);
+// });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::get('/users',function (Request $request) {
+    return User::all();
 });
