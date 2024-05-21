af4106e040ed68e8f7729db6a345cc22857b20d0
M-erick <muriithierick3758@gmail.com>
2024-05-21 11:42:30 +0300

    issue part

---
+++ app/Http/Controllers/IssueController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Issue;
+use Illuminate\Http\Request;
+
+class IssueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $issues = Issue::all();
+        return response()->json($issues);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'status' => 'required|string',
+        ]);
+
+        $issue = Issue::create($request->all());
+
+        return response()->json($issue, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $issue = Issue::findOrFail($id);
+        return response()->json($issue);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+        $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'status' => 'required|string',
+        ]);
+
+        $issue = Issue::findOrFail($id);
+        $issue->update($request->all());
+
+        return response()->json($issue, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+        $issue = Issue::findOrFail($id);
+        $issue->delete();
+
+        return response()->json(null, 204);
+    }
+}
