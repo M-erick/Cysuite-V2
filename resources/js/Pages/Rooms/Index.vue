2e1100c8ca82d1babd0ecbb4f666f2d654024a10
M-erick <muriithierick3758@gmail.com>
2024-05-22 15:11:51 +0300

    update the movie Link

--- resources/js/Pages/Rooms/Index.vue
+++ resources/js/Pages/Rooms/Index.vue
@@ -67,7 +67,8 @@
                                 </div>
                             </div>
                             <h3 class="flex justify-center items-center font-medium text-xl">
-                                <Link :href="'/rooms/' + room.id" class="flex justify-center items-center bg-opacity-80 z-10 top-0 left-0 w-full h-full text-black rounded-2xl">
+                                <!-- this routes should be dynamic :i'lll add '/rooms/details+ room.id' -->
+                                <Link :href="'/rooms/details'" class="flex justify-center items-center bg-opacity-80 z-10 top-0 left-0 w-full h-full text-black rounded-2xl">
                                     View All
                                     <svg class="ml-2 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"></path>
