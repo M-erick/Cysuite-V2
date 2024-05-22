cdc10606bb3b1a0b946d9ee20ddbab2fc29e351d
M-erick <muriithierick3758@gmail.com>
2024-05-22 15:51:48 +0300

    default rooms CRUD

---
+++ resources/js/Pages/Rooms/Create.vue
@@ -0,0 +1,41 @@
+<template>
+      <AuthenticatedLayout>
+  <div>
+    <h2>Create Room</h2>
+    <form @submit.prevent="createRoom">
+      <input v-model="newRoom.name" placeholder="Room Name" required>
+      <input v-model="newRoom.type" placeholder="Room Type" required>
+      <input v-model="newRoom.size" placeholder="Room Size" required>
+      <input v-model="newRoom.price" placeholder="Room Price" required>
+      <button type="submit">Create Room</button>
+    </form>
+  </div>
+
+
+
+
+</AuthenticatedLayout>
+</template>
+
+<script setup>
+import { reactive ,ref} from 'vue';
+import axios from 'axios';
+
+import { Link } from "@inertiajs/vue3";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+
+const newRoom = ref({
+  name: '',
+  type: '',
+  size: '',
+  price: ''
+});
+
+const createRoom = async () => {
+    // store the  data using post command
+  await axios.post('http://lr-cysuites.test/api/rooms', newRoom);
+};
+</script>
+<style>
+
+</style>
---
+++ resources/js/Pages/Rooms/Edit.vue
@@ -0,0 +1,47 @@
+<template>
+<AuthenticatedLayout>
+    <div>
+    <h2>Edit Room</h2>
+    <!-- <form @submit.prevent="updateRoom">
+      <input v-model="editRoom.name" placeholder="Room Name" required>
+      <input v-model="editRoom.type" placeholder="Room Type" required>
+      <input v-model="editRoom.size" placeholder="Room Size" required>
+      <input v-model="editRoom.price" placeholder="Room Price" required>
+      <button type="submit">Update Room</button>
+      <button @click="cancelEdit">Cancel</button>
+    </form> -->
+  </div>
+</AuthenticatedLayout>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { reactive,ref,onMounted } from 'vue';
+
+
+const editRoom = ref(null);
+const editing = ref(false);
+
+const updateRoom = async () => {
+  await axios.put(`http://lr-cysuites.test/api/rooms/${editRoom.value.id}`, editRoom.value);
+};
+
+const cancelEdit = () => {
+  editRoom.value = null;
+  editing.value = false;
+};
+
+const props = defineProps({
+  room: Object
+});
+
+onMounted(() => {
+  editRoom.value = props.room;
+  editing.value = true;
+});
+</script>
+
+<style>
+
+</style>
