86f940ce136752f976bb85c692584c0288a216eb
M-erick <muriithierick3758@gmail.com>
2024-05-22 10:15:37 +0300

    added font awesome icons

--- resources/js/Pages/Rooms/Details.vue
+++ resources/js/Pages/Rooms/Details.vue
@@ -5,6 +5,7 @@
             rel="stylesheet"
             href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css"
         />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
         <link
             rel="stylesheet"
             href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
@@ -112,31 +113,195 @@
                             class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 text-center px-2 mx-auto"
                         >
                             <div>
-                                <i class="fas fa-award"></i>
+                                <i class="fa-solid fa-bed"></i>
                                 <p>bedroom</p>
                             </div>
                             <div>
-                                <i class="fas fa-award"></i>
-                                <p>bedroom</p>
+                                <i class="fa-solid fa-fan"></i>
+                                <p>Air Conditioning</p>
                             </div>
                             <div>
-                                <i class="fas fa-award"></i>
-                                <p>bedroom</p>
+                                <i class="fa-solid fa-wifi"></i>
+                                <p>WiFi</p>
                             </div>
                             <div>
-                                <i class="fas fa-award"></i>
-                                <p>bedroom</p>
+                                <i class="fa-solid fa-tv"></i>
+                                <p>TV</p>
                             </div>
                             <div>
-                                <i class="fas fa-award"></i>
-                                <p>bedroom</p>
+                                <i class="fa-solid fa-phone"></i>
+                                <p>Telephone</p>
                             </div>
                             <div>
-                                <i class="fas fa-award"></i>
-                                <p>bedroom</p>
+                                <i class="fa-solid fa-bath"></i>
+                                <p>Bathrobes</p>
+                            </div>
+                            <div>
+                                <i class="fa-solid fa-mug-saucer"></i>
+                                <p>Coffee Machine</p>
+                            </div>
+                            <div>
+                                <i class="fa-solid fa-martini-glass"></i>
+                                <p>Mini-bar</p>
+                            </div>
+                            <div>
+                                <i class="fa-solid fa-restroom"></i>
+                                <p>Bath Room</p>
                             </div>
                         </div>
                     </article>
+                </div>
+
+                <!-- dummy form details:update on this later DevEric -->
+                <!-- component -->
+
+
+                <div class="max-w-2xl m-4">
+                    <h3 class="text-3xl font-medium mb-3">
+                        Additional Services
+                    </h3>
+                    <div
+                        class="p-4 max-w-md bg-white border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700"
+                    >
+                        <div class="flow-root">
+                            <ul
+                                role="list"
+                                class="divide-y divide-gray-200 dark:divide-gray-700"
+                            >
+                                <li class="py-3 sm:py-4">
+                                    <div class="flex items-center space-x-4">
+                                        <div class="flex-1 min-w-0">
+                                            <i class="fas fa-award"></i>
+
+                                            <p
+                                                class="text-sm font-medium text-gray-900 truncate dark:text-white"
+                                            >
+                                                Breakfast
+                                            </p>
+                                        </div>
+                                        <div
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
+                                        >
+                                            +$100
+                                            <span
+                                                class="text-sm font-medium text-gray-900"
+                                            >
+                                                /person/day</span
+                                            >
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="py-3 sm:py-4">
+                                    <div class="flex items-center space-x-4">
+                                        <div class="flex-1 min-w-0">
+                                            <i class="fas fa-award"></i>
+
+                                            <p
+                                                class="text-sm font-medium text-gray-900 truncate dark:text-white"
+                                            >
+                                                Half-board(breakfast + dinner or
+                                                lunch)
+                                            </p>
+                                        </div>
+                                        <div
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
+                                        >
+                                            +$120
+                                            <span
+                                                class="text-sm font-medium text-gray-900"
+                                            >
+                                                /person/day</span
+                                            >
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="py-3 sm:py-4">
+                                    <div class="flex items-center space-x-4">
+                                        <div class="flex-1 min-w-0">
+                                            <i class="fas fa-award"></i>
+                                            <p
+                                                class="text-sm font-medium text-gray-900 truncate dark:text-white"
+                                            >
+                                                Full-board(breakfast+ dinner+
+                                                lunch)
+                                            </p>
+                                        </div>
+                                        <div
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
+                                        >
+                                            $220
+                                            <span
+                                                class="text-sm font-medium text-gray-900"
+                                            >
+                                                /person/day</span
+                                            >
+                                        </div>
+                                    </div>
+                                </li>
+                                <h2 class="mx-auto p-5 font-semibold">
+                                    Other Services
+                                </h2>
+                                <li class="py-3 sm:py-4">
+                                    <div class="flex items-center space-x-4">
+                                        <div class="flex-1 min-w-0 mt-8">
+                                            <p
+                                                class="text-sm font-medium text-gray-900 truncate dark:text-white"
+                                            >
+                                                Wellness & Spa
+                                            </p>
+                                        </div>
+                                        <div
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
+                                        >
+                                            $20 /person/day
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="py-3 sm:py-4">
+                                    <div class="flex items-center space-x-4">
+                                        <div class="flex-1 min-w-0">
+                                            <p
+                                                class="text-sm font-medium text-gray-900 truncate dark:text-white"
+                                            >
+                                                Fitness Center
+                                            </p>
+                                        </div>
+                                        <div
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
+                                        >
+                                            +$15
+                                            <span
+                                                class="text-sm font-medium text-gray-900"
+                                            >
+                                                /person/day</span
+                                            >
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="py-3 sm:py-4">
+                                    <div class="flex items-center space-x-4">
+                                        <div class="flex-1 min-w-0">
+                                            <p
+                                                class="text-sm font-medium text-gray-900 truncate dark:text-white"
+                                            >
+                                                Parking
+                                            </p>
+                                        </div>
+                                        <div
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
+                                        >
+                                            +$12
+                                            <span
+                                                class="text-sm font-medium text-gray-900"
+                                            >
+                                                /person/day</span
+                                            >
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </section>
         </section>
@@ -144,7 +309,7 @@
 </template>
 
 <script>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 export default {};
 </script>
