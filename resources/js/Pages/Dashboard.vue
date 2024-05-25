6e7c71994153d12d22ac70dddd10ce8415e401af
M-erick <muriithierick3758@gmail.com>
2024-05-25 14:02:35 +0300

    modified the dashboard to be dynamic

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,28 +1,156 @@
-<script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head ,Link} from '@inertiajs/vue3';
-</script>
-
-<!--  When a user logins he is redirected to this page  -->
 <template>
-    <Head title="Dashboard" />
-
     <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
-        </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+        <div class="relative pt-2 lg:pt-2 ">
+            <article
+                    class="bg-white  p-6 mb-6 shadow  group  rounded-2xl cursor-pointer border">
+                    <a target="_self" href="/blog/slug"
+                        class="absolute opacity-0 top-0 right-0 left-0 bottom-0"></a>
+                    <div class="relative mb-4 rounded-2xl">
+                        <img class="max-h-80 rounded-2xl w-full object-cover  "
+                            src="https://images.pexels.com/photos/163097/twitter-social-media-communication-internet-network-163097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="">
+
+
+
+                    </div>
+
+
+                    <div>
+                    </div>
+                </article>
+
+            <article
+                class="bg-white  p-6  shadow transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer border">
+
+
+                <div class=" w-full pb-4 mb-auto">
+                    <div class="flex items-center">
+
+                        <div class="flex-1">
+                            <div class="flex items-center justify-center">
+
+                                    <p class="text-3xl  ">Booked Rooms</p>
+
+
+                            </div>
+                            <p class="text-sm font-semibold ">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Quaerat, quo.</p>
+
+                        </div>
+                    </div>
+
                 </div>
-            </div>
-            <div>
-                <!-- <Link href={route('admins.index')} class="text-blue-500">View Admin List</Link> -->
-                <Link :href="route('admins.create')" class="text-blue-500">Create Admin</Link>
 
+                <div>
+                </div>
+            </article>
+
+
+        </div>
+        <div class="bg-cover w-full flex justify-center items-center"
+            style="background-image: url('/images/mybackground.jpeg');">
+            <div class="w-full bg-white p-5 bg-opacity-40 backdrop-filter backdrop-blur-lg">
+                <div class="w-12/12 mx-auto rounded-2xl bg-white p-5 bg-opacity-40 backdrop-filter backdrop-blur-lg">
+                    <div v-if="roomFetched" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 text-center px-2 mx-auto">
+                        <article v-for="room in roomData" :key="room.id"
+                            class="bg-white p-6 mb-6 shadow transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer border">
+                            <div class="relative mb-4 rounded-2xl">
+
+                                <!-- I'll implement the  image part -->
+                                <img class="max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105"
+                                    :src="getImageUrl(room.image) || 'https://via.placeholder.com/150'"
+                                    alt="Room Image">
+                            </div>
+                            <div class="w-full pb-4 mb-auto">
+                                <div class="flex items-center">
+                                    <div class="flex flex-1">
+                                        <div class="">
+                                            <p class="text-3xl">{{ room.type }} Room </p>
+
+                                            <!-- define the description part in the database {i need : description ,image,capacity} -->
+                                            <p class="text-sm font-semibold">{{ room.description }}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <h3 class="flex justify-center items-center font-medium text-xl">
+                                <!-- this routes should be dynamic :i'lll add '/rooms/details+ room.id' -->
+                                <Link :href="'/rooms/details/' + room.id"
+                                    class="flex justify-center items-center bg-opacity-80 z-10 top-0 left-0 w-full h-full text-black rounded-2xl">
+                                View All
+                                <i
+                                                class="fa-solid fa-chevron-right pl-2"
+                                            ></i>
+                                </Link>
+                            </h3>
+                        </article>
+                    </div>
+                    <div v-else>
+                        <p>Loading ...</p>
+
+
+                    </div>
+                </div>
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { ref, onMounted } from 'vue';
+import { usePage } from "@inertiajs/vue3";
+import axios from 'axios';
+
+// user data
+const { props } = usePage();
+const currentUser = props.auth.user;
+// console.log(currentUser);
+const roomFetched = ref(false);
+onMounted(async() => {
+    await fetchUserBookings();
+
+});
+
+
+ const roomData = ref([]);
+
+ const fetchUserBookings = async () => {
+  try {
+    const response = await axios.get("/api/guest_rooms");
+    const guestRoomData = response.data;
+    for (const guest of guestRoomData) {
+      const guestBooking = await axios.get(`/api/users/${guest.user_id}`);
+      //  user's role and assign it to admin object
+      guest.username = guestBooking.data.name;
+      guest.useremail = guestBooking.data.email;
+      guest.dateAssigned = guestBooking.data.created_at;
+
+      const assignedRooms = await axios.get(`/api/rooms/${guest.room_id}`);
+      guest.assignedRooms = assignedRooms.data.name;
+      guest.image = assignedRooms.data.image;
+      guest.description = assignedRooms.data.description;
+
+    }
+     // Filter out guest bookings for the current user
+     const currentUserBookings = guestRoomData.filter(guest => guest.user_id === currentUser.id);
+
+    console.log(currentUserBookings);
+    roomFetched.value = true;
+    roomData.value = currentUserBookings;
+  } catch (error) {
+    console.error("error fetching data details", error);
+  }
+};
+
+
+
+// to get the image :try this logic
+function getImageUrl(imagePath) {
+    // i'll create a tenary statement if-else
+    return imagePath ? `/storage/${imagePath}` : 'https://via.placeholder.com/150';
+}
+</script>
+
+<style></style>
