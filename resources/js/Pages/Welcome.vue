16e64119dcee72661b4129e2399feac57f9444e4
M-erick <muriithierick3758@gmail.com>
2024-05-25 19:40:24 +0300

    styled the landing Page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 
 defineProps({
     canLogin: {
@@ -34,29 +35,32 @@ defineProps({
 <section class="relative  bg-blueGray-50">
 <div class="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
         <div class="absolute top-0 w-full h-full bg-center bg-cover" style="
-            background-image: url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1267&amp;q=80');
+            background-image: url('https://www.quellenhof.it/fileadmin/_processed_/6/2/csm_Quellenhof_Resort_Passeier_Eingang_3_6931ccbc0d.jpg');
           ">
           <span id="blackOverlay" class="w-full h-full absolute opacity-75 bg-black"></span>
         </div>
+
+
         <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
+
             <Link
                 v-if="$page.props.auth.user"
                 :href="route('dashboard')"
-                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                class="font-semibold text-gray-600    focus:outline focus:outline-2 focus:rounded-sm focus:outline-white"  style="font-family: 'Roboto Serif',serif ;font-weight: 600;font-style: normal;"
                 >Dashboard</Link
             >
 
             <template v-else>
                 <Link
                     :href="route('login')"
-                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                    class="font-semibold text-white    focus:outline focus:outline-2 focus:rounded-sm focus:outline-white" style="font-family: 'Roboto Serif',serif ;font-weight: 600;font-style: normal;"
                     >Log in</Link
                 >
 
                 <Link
                     v-if="canRegister"
                     :href="route('register')"
-                    class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                    class="ms-4 font-semibold  text-white   focus:outline focus:outline-2 focus:rounded-sm focus:outline-white" style="font-family: 'Roboto Serif',serif ;font-weight: 600;font-style: normal;"
                     >Register</Link
                 >
             </template>
@@ -64,22 +68,28 @@ defineProps({
         <div class="container relative mx-auto">
           <div class="items-center flex flex-wrap">
             <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+
+
+
               <div class="pr-12">
-                <h1 class="text-white font-semibold text-5xl">
-                  Your story starts with us.
+
+                <h1 class="text-white font-semibold text-5xl pb-4" style="font-family: 'Cinzel Decorative',serif ;font-weight: 900;font-style: normal; color:#AD9551;">
+                 Cysuite
                 </h1>
-                <p class="mt-4 text-lg text-blueGray-200">
-                 Best ranked rooms
+
+
+
+                <h1 class="text-white font-semibold text-2xl" style="font-family: 'Cinzel Decorative',serif ;font-weight: 700;font-style: normal;">
+                  About Us
+                </h1>
+                <p class="mt-10 text-sm text-blueGray-200">
+                    <i class="fa-solid fa-chevron-down" style="font-size: 30px"></i>
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px" style="transform: translateZ(0px)">
-          <svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
-            <polygon class="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
-          </svg>
-        </div>
+
       </div>
       <section class="pb-10 bg-blueGray-200 -mt-24">
         <div class="container mx-auto px-4">
@@ -87,12 +97,16 @@ defineProps({
             <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
               <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                 <div class="px-4 py-5 flex-auto">
-                  <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
-                    <i class="fas fa-award"></i>
+                  <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full " style=" background-color:#AD9551;">
+
+                    <i class="fas fa-fingerprint"></i>
                   </div>
-                  <h6 class="text-xl font-semibold">Awarded Agency</h6>
+                  <h6 class="text-xl font-semibold" style="font-family: 'Roboto Serif',serif ;font-weight: 600;font-style: normal;">About Us </h6>
                   <p class="mt-2 mb-4 text-blueGray-500">
-                    since 2024
+                    Cysuite has been welcoming guests to spectacular destinations in the most stunning style.
+                    Together we’ve explored the world and discovered new places, possibilities and horizons.
+                     The very epitome of luxury, our brand is passionate about creating and crafting rich,
+                     meaningful and memorable experiences for every guest.
                   </p>
                 </div>
               </div>
@@ -100,12 +114,13 @@ defineProps({
             <div class="w-full md:w-4/12 px-4 text-center">
               <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                 <div class="px-4 py-5 flex-auto">
-                  <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
-                    <i class="fas fa-retweet"></i>
+                  <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full" style=" background-color:#AD9551;">
+
+                    <i class="fas fa-award"></i>
                   </div>
-                  <h6 class="text-xl font-semibold">Free Revisions</h6>
+                  <h6 class="text-xl font-semibold" style="font-family: 'Roboto Serif',serif ;font-weight: 600;font-style: normal;">Our Story</h6>
                   <p class="mt-2 mb-4 text-blueGray-500">
-                    Revised lastly 2024
+                    Across decades  innovation and discovery has always guided the evolution of the Cysuite brand.
                   </p>
                 </div>
               </div>
@@ -113,12 +128,14 @@ defineProps({
             <div class="pt-6 w-full md:w-4/12 px-4 text-center">
               <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                 <div class="px-4 py-5 flex-auto">
-                  <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
-                    <i class="fas fa-fingerprint"></i>
+                  <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full" style=" background-color:#AD9551;">
+                    <i class="fas fa-retweet"></i>
                   </div>
-                  <h6 class="text-xl font-semibold">Verified Company</h6>
+                  <h6 class="text-xl font-semibold" style="font-family: 'Roboto Serif',serif ;font-weight: 600;font-style: normal;">Review</h6>
                   <p class="mt-2 mb-4 text-blueGray-500">
-                    Recognized company
+                    At the heart of Cysuite is the remarkable people who look after each and every guest attentively and thoughtfully.
+
+                      we have a team  waiting to welcome you and take care of everything you could wish for with warmth, charisma, care and attention to detail.
                   </p>
                 </div>
               </div>
