4d7314eaaaa7bafa517b15da4dd464fc3dd945b0
M-erick <muriithierick3758@gmail.com>
2024-05-25 06:33:05 +0300

    managed to fetch guest details from db

--- resources/js/Pages/Admin/Panel.vue
+++ resources/js/Pages/Admin/Panel.vue
@@ -30,14 +30,14 @@
 
                         <Link :href="route('rooms.create')"
                             class="block text-gray-500 py-2.5 px-4 my-4 rounded hover:bg-green-900 hover:text-white">
-                             <i class="fa-solid fa-house mr-2"></i>Add Room
+                        <i class="fa-solid fa-house mr-2"></i>Add Room
                         </Link>
 
 
 
                         <Link :href="route('response')"
                             class="block text-gray-500 py-2.5 px-4 my-4 rounded hover:bg-green-900 hover:text-white">
-                             <i class="fas fa-exchange-alt mr-2"></i>Response
+                        <i class="fas fa-exchange-alt mr-2"></i>Response
                         </Link>
 
 
@@ -48,33 +48,23 @@
                 <div class="flex-1 p-4 w-full md:w-1/2">
                     <div class="mt-8 flex flex-wrap space-x-0 space-y-2 md:space-x-4 md:space-y-0">
                         <div class="flex-1 bg-white p-4 shadow rounded-lg md:w-1/2">
-                            <h2 class="text-gray-500 text-lg font-semibold pb-1">
+                            <h2 class="text-gray-500 text-lg font-semibold pb-1" style="color: #046a5b;">
                                 Users
                             </h2>
-                            <hr class="font-semibold mt-3" />
-                            <div style="
-                                    position: relative;
-                                    height: 150px;
-                                    width: 100%;
-                                ">
-                                <p class="">{{ totalUsers }}</p>
+                            <hr class="font-semibold mt-3" style="border-color:#046a5b" />
+                            <div class="flex items-center justify-center" style="height: 150px;">
+                                <p class="text-center font-bold text-5xl">{{ totalUsers }}</p>
                             </div>
                         </div>
 
+
                         <div class="flex-1 bg-white p-4 shadow rounded-lg md:w-1/2">
-                            <h2 class="text-gray-500 text-lg font-semibold pb-1">
-                                Rooms
+                            <h2 class="text-gray-500 text-lg font-semibold pb-1" style="color: #046a5b;">
+                              Available Rooms
                             </h2>
-                            <hr class="font-semibold mt-3" />
-                            <div style="
-                                    position: relative;
-                                    height: 150px;
-                                    width: 100%;
-                                ">
-                                <p>
-                                    work on adding Room count which admin is
-                                    managing
-                                </p>
+                            <hr class="font-semibold mt-3" style="border-color:#046a5b" />
+                            <div class="flex items-center justify-center" style="height: 150px;">
+                                <p class="text-center font-bold text-5xl">{{ availableRooms }}</p>
                             </div>
                         </div>
 
@@ -90,71 +80,40 @@
                         <table class="w-full table-auto text-sm">
                             <thead>
                                 <tr class="text-sm leading-normal">
-                                    <th class="py-2 px-4 font-bold uppercase text-sm border-b">
+                                    <th class="py-2 px-4 font-bold uppercase text-white text-sm border-b">
+                                      ID
+                                    </th>
+                                    <th class="py-2 px-4 font-bold uppercase text-white  text-sm border-b">
                                         Name
                                     </th>
-                                    <th class="py-2 px-4 font-bold uppercase text-sm border-b">
-                                        Room
+                                    <th class="py-2 px-4 font-bold uppercase text-white text-sm border-b">
+                                        Email
                                     </th>
-                                    <th class="py-2 px-4 font-bold uppercase text-sm border-b">
+                                    <th class="py-2 px-4 font-bold uppercase text-white  text-sm border-b">
                                         Date In
                                     </th>
-                                    <th class="py-2 px-4 font-bold uppercase text-sm border-b">
+                                    <th class="py-2 px-4 font-bold uppercase  text-white text-sm border-b">
                                         Date Out
                                     </th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td class="py-2 px-4 border-b">Erick</td>
-                                    <td class="py-2 px-4 border-b">F31</td>
+                                <tr v-for="guest in guestData" :key="guest.id">
+                                    <td class="py-2 px-4 border-b">{{guest.id}}</td>
+
+                                    <td class="py-2 px-4 border-b">{{guest.name}}</td>
+                                    <td class="py-2 px-4 border-b">{{ guest.email }}</td>
                                     <td class="py-2 px-4 border-b">
-                                        27/07/2023
+                                        {{ formatTimestamp(guest.created_at) }}
                                     </td>
-                                    <td class="py-2 px-4 border-b">
-                                        27/07/2023
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="py-2 px-4 border-b">Ian</td>
-                                    <td class="py-2 px-4 border-b">F32</td>
-                                    <td class="py-2 px-4 border-b">
-                                        27/07/2023
-                                    </td>
-                                    <td class="py-2 px-4 border-b">
-                                        27/07/2023
+                                    <td class="py-2 px-4 border-b font-bold " style="color:#046a5b">
+                                      PENDING..
                                     </td>
                                 </tr>
-                                <tr>
-                                    <td class="py-2 px-4 border-b">Terry</td>
-                                    <td class="py-2 px-4 border-b">F33</td>
-                                    <td class="py-2 px-4 border-b">
-                                        27/07/2023
-                                    </td>
-                                    <td class="py-2 px-4 border-b">
-                                        27/07/2023
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="py-2 px-4 border-b">Chelsea</td>
-                                    <td class="py-2 px-4 border-b">F34</td>
-                                    <td class="py-2 px-4 border-b">
-                                        27/07/2023
-                                    </td>
-                                    <td class="py-2 px-4 border-b">
-                                        27/07/2023
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="py-2 px-4 border-b">Fred</td>
-                                    <td class="py-2 px-4 border-b">F35</td>
-                                    <td class="py-2 px-4 border-b">
-                                        27/07/2023
-                                    </td>
-                                    <td class="py-2 px-4 border-b">
-                                        27/07/2023
-                                    </td>
-                                </tr>
+
+
+
+
                             </tbody>
                         </table>
 
@@ -261,6 +220,8 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
 const totalUsers = ref(0);
+const availableRooms = ref(0);
+const guestData = ref([]);
 const { props } = usePage();
 const currentUser = props.auth.user;
 // use the name of the currently log in user:i see this as repeating my self:already have user:
@@ -273,8 +234,43 @@ const fetchuser = async()=>{
         console.error('Error fetching total users:', error);
     }
 };
+
+// fetch available rooms
+const fetchRoom = async () => {
+    try {
+        const response = await axios.get('/api/rooms');
+        const roomData = response.data;
+
+        // filter the fetched data using :Filter Hollah
+        const availableRoom = roomData.filter(room => !room.is_occupied);
+        availableRooms.value = availableRoom.length;
+
+
+    } catch (error) {
+        console.error('error fetching available rooms', error);
+    }
+};
+const fetchAdmins = async()=>{
+    try{
+        const response = await axios.get('/api/guest');
+        guestData.value = response.data;
+
+        console.log(guestData.value);
+
+    }catch(error){
+        console.error('error fetching admin details',error);
+    }
+
+};
+const formatTimestamp = (timestamp) => {
+  const date = new Date(timestamp);
+  const options = { hour: 'numeric', minute: 'numeric', day: 'numeric', month: 'long', year: 'numeric' };
+  return date.toLocaleString(undefined, options);
+};
 onMounted(async()=>{
     await fetchuser();
+    await fetchRoom();
+    await fetchAdmins();
 })
 </script>
 
