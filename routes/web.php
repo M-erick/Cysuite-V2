65e2a46cbcebbe6ffe34fa7886cda8fe9f24637b
M-erick <muriithierick3758@gmail.com>
2024-05-27 12:16:36 +0300

    commented the canRegister,route am planning on using this.

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
-        // 'canRegister' => Route::has('register'),
+        'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
