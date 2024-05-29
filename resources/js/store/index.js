3ee754a46d89d0e944fbabc97456fe99d7750b74
M-erick <muriithierick3758@gmail.com>
2024-05-29 13:45:07 +0300

    setting  up the adminPanel  store

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,11 +1,14 @@
 import { createStore } from 'vuex';
 import issues from './modules/issues';
 import rooms from './modules/rooms';
+import adminPanel from './modules/adminPanel';
+
 const store = createStore({
     // i should register issue
   modules: {
     issues,
     rooms,
+    adminPanel,
   },
 });
 
