7c88cbe8f12112df171819cb4e827fda2e824ed8
M-erick <muriithierick3758@gmail.com>
2024-06-10 19:39:59 +0300

    Rearranged the components in Components/RoomComponent

---
+++ resources/js/Components/RoomComponents/Carousel.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- Create a  carousel of random hotel images then auto-move the images -->
+    <article class="bg-white  mt-2 ml-3 mr-3 mb-3   group   cursor-pointer border">
+
+        <div class="relative  ">
+            <img class="max-h-80  w-full object-cover  "
+                src="https://plus.unsplash.com/premium_photo-1661902745675-11846e4f9a41?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="">
+        </div>
+
+        <div>
+        </div>
+    </article>
+</template>
--- resources/js/Pages/Rooms/Index.vue
+++ resources/js/Pages/Rooms/Index.vue
@@ -3,24 +3,8 @@
 
         <div class="relative pt-2 lg:pt-2 ">
 
-            <article class="bg-white  mt-2 ml-3 mr-3 mb-3   group   cursor-pointer border">
-
-                <div class="relative  ">
-                    <img class="max-h-80  w-full object-cover  "
-                        src="https://plus.unsplash.com/premium_photo-1661902745675-11846e4f9a41?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        alt="">
-
-
-
-                </div>
-
-
-                <div>
-                </div>
-            </article>
+           <Carousel/>
             <article class="bg-white  p-6  ">
-
-
                 <div class=" w-full pb-4 mb-auto">
                     <div class="flex items-center">
 
@@ -30,22 +14,13 @@
                                 <p class="text-3xl  "
                                     style="color:#AD9551;font-family: 'Cinzel Decorative',serif ;font-weight: 700;font-style: normal;">
                                     Rooms</p>
-
-
-
                             </div>
                             <p class="text-center px-2 font-semibold "
                                 style=" font-family: 'Roboto Serif', serif;font-style: normal;font-weight:200;">
                                 Discover Our thoughtfully designed apartments </p>
-
-
-
                         </div>
                     </div>
 
-                </div>
-
-                <div>
                 </div>
             </article>
 
@@ -56,7 +31,7 @@
             <div class="w-full bg-white p-5 bg-opacity-40 backdrop-filter backdrop-blur-lg">
                 <div class="w-12/12 mx-auto rounded-2xl bg-white p-5 bg-opacity-40 backdrop-filter backdrop-blur-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 text-center px-2 mx-auto">
-                        <RoomCard :rooms ="rooms"/>
+                        <RoomCard :rooms="rooms" />
                     </div>
                 </div>
             </div>
@@ -67,7 +42,8 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { ref, onMounted } from 'vue';
-import RoomCard from '@/Components/RoomCard.vue';
+import RoomCard from '@/Components/RoomComponents/RoomCard.vue';
+import Carousel from '@/Components/RoomComponents/Carousel.vue';
 import axios from 'axios';
 
 onMounted(() => {
@@ -83,7 +59,6 @@ const fetchRooms = async () => {
         // default routes :http://lr-cysuites.test/api/rooms
         const response = await axios.get('/api/rooms');
         rooms.value = response.data;
-        console.log(rooms.value);
     } catch (error) {
         console.error('Error fetching rooms:', error);
     }
