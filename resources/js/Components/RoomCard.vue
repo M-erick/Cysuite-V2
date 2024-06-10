1e60c6e3119da67f60933e472cd97c3047f2efa5
M-erick <muriithierick3758@gmail.com>
2024-06-10 19:30:04 +0300

    Redefined the ViewAll button as a component

--- resources/js/Components/RoomCard.vue
+++ resources/js/Components/RoomCard.vue
@@ -23,20 +23,14 @@
                 </div>
             </div>
         </div>
-        <h3 class="flex justify-center items-center font-medium text-xl">
-            <!-- this routes should be dynamic :i'lll add '/rooms/details+ room.id' -->
-            <Link :href="'/rooms/details/' + room.id"
-                class="flex justify-center items-center bg-opacity-80 z-10 top-0 left-0 w-full h-full rounded-2xl"
-                style="color:#AD9551 ; font-family: 'Roboto Serif', serif;font-style: normal;">
-            View All
-            <i class="fa-solid fa-chevron-right pl-2"></i>
-            </Link>
-        </h3>
+
+        <VieAllButton :room="room"/>
+
     </article>
 </template>
 
 <script setup>
-import { Link } from "@inertiajs/vue3";
+import VieAllButton from './VieAllButton.vue';
 const { rooms } = defineProps({
     rooms: {
         require: true,
---
+++ resources/js/Components/VieAllButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <h3 class="flex justify-center items-center font-medium text-xl">
+        <!-- this routes should be dynamic :i'lll add '/rooms/details+ room.id' -->
+        <Link :href="'/rooms/details/' + room.id"
+            class="flex justify-center items-center bg-opacity-80 z-10 top-0 left-0 w-full h-full rounded-2xl"
+            style="color:#AD9551 ; font-family: 'Roboto Serif', serif;font-style: normal;">
+        View All
+        <i class="fa-solid fa-chevron-right pl-2"></i>
+        </Link>
+    </h3>
+</template>
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+const { room } = defineProps({
+    room: {
+        require: true,
+    }
+});
+</script>
