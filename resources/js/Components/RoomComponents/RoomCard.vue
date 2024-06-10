ff04e6b82b0d1a547758c56c734433221a661f45
M-erick <muriithierick3758@gmail.com>
2024-06-10 20:18:02 +0300

    the room card handles the whole data instead of using props

--- resources/js/Components/RoomComponents/RoomCard.vue
+++ resources/js/Components/RoomComponents/RoomCard.vue
@@ -3,11 +3,10 @@
     <article v-for="room in rooms" :key="room.id"
         class="bg-white p-3 mb-3 shadow transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer border">
         <div class="relative mb-4 rounded-2xl">
-
-            <!-- I'll implement the  image part -->
             <img class="max-h-80 rounded w-full object-cover transition-transform duration-300 transform group-hover:scale-105"
                 :src="getImageUrl(room.image) || 'https://via.placeholder.com/150'" alt="Room Image">
         </div>
+        
         <div class="w-full pb-4 mb-auto">
             <div class="flex items-center">
                 <div class="flex flex-1">
@@ -31,18 +30,30 @@
 
 <script setup>
 import VieAllButton from './VieAllButton.vue';
-const { rooms } = defineProps({
-    rooms: {
-        require: true,
-        type: Array,
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
+const rooms = ref([]);
+
+const fetchRooms = async () => {
+    try {
+
+        // default routes :http://lr-cysuites.test/api/rooms
+        const response = await axios.get('/api/rooms');
+        rooms.value = response.data;
+    } catch (error) {
+        console.error('Error fetching rooms:', error);
     }
-});
+};
+
 // to get the image :try this logic
 function getImageUrl(imagePath) {
     // i'll create a tenary statement if-else
-  return imagePath ? `/storage/${imagePath}` : 'https://via.placeholder.com/150';
+    return imagePath ? `/storage/${imagePath}` : 'https://via.placeholder.com/150';
 }
+
+onMounted(() => {
+    fetchRooms();
+});
 </script>
 
-<style></style>
