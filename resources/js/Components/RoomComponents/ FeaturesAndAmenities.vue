5b89530bd365ed49e432151686c4c69f84ec2c3c
M-erick <muriithierick3758@gmail.com>
2024-06-10 20:36:06 +0300

    Transformed the feature  into a seperate component,I'll make this dynamic

---
+++ resources/js/Components/RoomComponents/ FeaturesAndAmenities.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h3 class="font-bold text-3xl">Features & Amenities</h3>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 text-center px-2 mx-auto">
+            <div>
+                <i class="fa-solid  fa_color fa-bed" ></i>
+                <p>bedroom</p>
+            </div>
+            <div>
+                <i class="fa-solid  fa_color fa-fan" ></i>
+                <p>Air Conditioning</p>
+            </div>
+            <div>
+                <i class="fa-solid fa_color  fa-wifi" ></i>
+                <p>WiFi</p>
+            </div>
+            <div>
+                <i class="fa-solid  fa_color fa-tv" ></i>
+                <p>TV</p>
+            </div>
+            <div>
+                <i class="fa-solid fa_color  fa-phone" ></i>
+                <p>Telephone</p>
+            </div>
+            <div>
+                <i class="fa-solid  fa_color fa-bath" ></i>
+                <p>Bathrobes</p>
+            </div>
+            <div>
+                <i class="fa-solid  fa_color fa-mug-saucer" ></i>
+                <p>Coffee Machine</p>
+            </div>
+            <div>
+                <i class="fa-solid fa_color  fa-martini-glass" ></i>
+                <p>Mini-bar</p>
+            </div>
+            <div>
+                <i class="fa-solid  fa_color fa-restroom" ></i>
+                <p>Bath Room</p>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+.fa_color {
+    color: #AD9551
+}
+</style>
--- resources/js/Pages/Rooms/Details.vue
+++ resources/js/Pages/Rooms/Details.vue
@@ -3,25 +3,15 @@
         <!-- component -->
 
         <section class="relative bg-blueGray-50">
-            <div
-                class="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75"
-            >
-                <div
-                    class="absolute top-0 w-full h-full bg-center bg-cover"
-                    :style="{
-                        backgroundImage: `url(${getImageUrl(room.image)})`,
-                    }"
-                >
-                    <span
-                        id="blackOverlay"
-                        class="w-full h-full absolute opacity-50 bg-black"
-                    ></span>
+            <div class="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
+                <div class="absolute top-0 w-full h-full bg-center bg-cover" :style="{
+                    backgroundImage: `url(${getImageUrl(room.image)})`,
+                }">
+                    <span id="blackOverlay" class="w-full h-full absolute opacity-50 bg-black"></span>
                 </div>
                 <div class="container relative mx-auto">
                     <div class="items-center flex flex-wrap">
-                        <div
-                            class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center"
-                        >
+                        <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
                             <div class="pr-12">
                                 <h1 class="text-white font-semibold text-5xl">
                                     {{ room.name }}
@@ -33,43 +23,26 @@
                         </div>
                     </div>
                 </div>
-                <div
-                    class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
-                    style="transform: translateZ(0px)"
-                >
-                    <svg
-                        class="absolute bottom-0 overflow-hidden"
-                        xmlns="http://www.w3.org/2000/svg"
-                        preserveAspectRatio="none"
-                        version="1.1"
-                        viewBox="0 0 2560 100"
-                        x="0"
-                        y="0"
-                    >
-                        <polygon
-                            class="text-blueGray-200 fill-current"
-                            points="2560 0 2560 100 0 100"
-                        ></polygon>
-                    </svg>
-                </div>
+
             </div>
             <section class="pb-10 bg-white -mt-24">
                 <article
-                    class="bg-white p-6 mb-6 shadow group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer border"
-                >
+                    class="bg-white p-6 mb-6 shadow group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer border">
                     <div class="w-full pb-4 mb-auto">
                         <div class="flex items-center">
                             <div class=" flex-1">
                                 <div class="flex items-center justify-center">
                                     <!-- <p class="text-3xl">{{ room.name }}</p> -->
-                                    <p class="text-sm font-semibold" style=" font-family: 'Roboto Serif', serif;font-style: normal;font-weight:400;">
+                                    <p class="text-sm font-semibold"
+                                        style=" font-family: 'Roboto Serif', serif;font-style: normal;font-weight:400;">
                                         {{ room.description }}
                                     </p>
 
                                 </div>
-                                <p class="text-center px-2  pt-3 font-semibold" style="color:#AD9551; font-family: 'Cinzel Decorative',serif ;font-style: normal;font-weight:700;">
-                                        Price: ksh {{ room.price }}
-                                    </p>
+                                <p class="text-center px-2  pt-3 font-semibold"
+                                    style="color:#AD9551; font-family: 'Cinzel Decorative',serif ;font-style: normal;font-weight:700;">
+                                    Price: ksh {{ room.price }}
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -89,66 +62,21 @@
                         </button>
                     </div> -->
                 </article>
-                <div
-                    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 text-center items-center px-2 mx-auto"
-                >
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 text-center items-center px-2 mx-auto">
                     <article class="mb-6 shadow">
                         <div class="relative h-full w-full">
-                            <img
-                                class="h-full w-full object-cover"
-                                :src="getImageUrl(room.image)"
-                                alt=""
-                            />
+                            <img class="h-full w-full object-cover" :src="getImageUrl(room.image)" alt="" />
                         </div>
                     </article>
                     <!-- create a grid inside the the article below to contain room feature amenities  -->
                     <article class="bg-white p-6 mb-6">
 
-                        <p class="text-center   pb-3 font-semibold" style="color:#AD9551; font-family: 'Cinzel Decorative',serif ;font-style: normal;font-weight:700;font-size: 20px;">
+                        <p class="text-center   pb-3 font-semibold"
+                            style="color:#AD9551; font-family: 'Cinzel Decorative',serif ;font-style: normal;font-weight:700;font-size: 20px;">
                             {{ room.type }}
-                                    </p>
-                        <h3 class="font-bold text-3xl">Features & Amenities</h3>
+                        </p>
+                        <FeaturesAndAmenities/>
 
-                        <div
-                            class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 text-center px-2 mx-auto"
-                        >
-                            <div >
-                                <i class="fa-solid fa-bed" style="color:#AD9551"></i>
-                                <p>bedroom</p>
-                            </div>
-                            <div>
-                                <i class="fa-solid fa-fan" style="color:#AD9551"></i>
-                                <p>Air Conditioning</p>
-                            </div>
-                            <div>
-                                <i class="fa-solid fa-wifi" style="color:#AD9551"></i>
-                                <p>WiFi</p>
-                            </div>
-                            <div>
-                                <i class="fa-solid fa-tv" style="color:#AD9551"></i>
-                                <p>TV</p>
-                            </div>
-                            <div>
-                                <i class="fa-solid fa-phone" style="color:#AD9551"></i>
-                                <p>Telephone</p>
-                            </div>
-                            <div>
-                                <i class="fa-solid fa-bath" style="color:#AD9551"></i>
-                                <p>Bathrobes</p>
-                            </div>
-                            <div>
-                                <i class="fa-solid fa-mug-saucer" style="color:#AD9551"></i>
-                                <p>Coffee Machine</p>
-                            </div>
-                            <div>
-                                <i class="fa-solid fa-martini-glass" style="color:#AD9551"></i>
-                                <p>Mini-bar</p>
-                            </div>
-                            <div>
-                                <i class="fa-solid fa-restroom" style="color:#AD9551"></i>
-                                <p>Bath Room</p>
-                            </div>
-                        </div>
                     </article>
                 </div>
 
@@ -159,34 +87,23 @@
                     <h3 class="text-3xl font-medium mb-3">
                         Additional Services
                     </h3>
-                    <div
-                        class="p-4 max-w-md bg-white border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700"
-                    >
+                    <div class="p-4 max-w-md bg-white border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                         <div class="flow-root">
-                            <ul
-                                role="list"
-                                class="divide-y divide-gray-200 dark:divide-gray-700"
-                            >
+                            <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
                                 <li class="py-3 sm:py-4">
                                     <div class="flex items-center space-x-4">
                                         <div class="flex-1 min-w-0">
                                             <i class="fas fa-award" style="color:#AD9551"></i>
 
-                                            <p
-                                                class="text-sm font-medium text-gray-900 truncate dark:text-white"
-                                            >
+                                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
                                                 Breakfast
                                             </p>
                                         </div>
                                         <div
-                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
-                                        >
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
                                             +$100
-                                            <span
-                                                class="text-sm font-medium text-gray-900"
-                                            >
-                                                /person/day</span
-                                            >
+                                            <span class="text-sm font-medium text-gray-900">
+                                                /person/day</span>
                                         </div>
                                     </div>
                                 </li>
@@ -195,22 +112,16 @@
                                         <div class="flex-1 min-w-0">
                                             <i class="fas fa-award" style="color:#AD9551"></i>
 
-                                            <p
-                                                class="text-sm font-medium text-gray-900 truncate dark:text-white"
-                                            >
+                                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
                                                 Half-board(breakfast + dinner or
                                                 lunch)
                                             </p>
                                         </div>
                                         <div
-                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
-                                        >
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
                                             +$120
-                                            <span
-                                                class="text-sm font-medium text-gray-900"
-                                            >
-                                                /person/day</span
-                                            >
+                                            <span class="text-sm font-medium text-gray-900">
+                                                /person/day</span>
                                         </div>
                                     </div>
                                 </li>
@@ -218,22 +129,16 @@
                                     <div class="flex items-center space-x-4">
                                         <div class="flex-1 min-w-0">
                                             <i class="fas fa-award" style="color:#AD9551"></i>
-                                            <p
-                                                class="text-sm font-medium text-gray-900 truncate dark:text-white"
-                                            >
+                                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
                                                 Full-board(breakfast+ dinner+
                                                 lunch)
                                             </p>
                                         </div>
                                         <div
-                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
-                                        >
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
                                             $220
-                                            <span
-                                                class="text-sm font-medium text-gray-900"
-                                            >
-                                                /person/day</span
-                                            >
+                                            <span class="text-sm font-medium text-gray-900">
+                                                /person/day</span>
                                         </div>
                                     </div>
                                 </li>
@@ -243,15 +148,12 @@
                                 <li class="py-3 sm:py-4">
                                     <div class="flex items-center space-x-4">
                                         <div class="flex-1 min-w-0 mt-8">
-                                            <p
-                                                class="text-sm font-medium text-gray-900 truncate dark:text-white"
-                                            >
+                                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
                                                 Wellness & Spa
                                             </p>
                                         </div>
                                         <div
-                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
-                                        >
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
                                             $20 /person/day
                                         </div>
                                     </div>
@@ -259,42 +161,30 @@
                                 <li class="py-3 sm:py-4">
                                     <div class="flex items-center space-x-4">
                                         <div class="flex-1 min-w-0">
-                                            <p
-                                                class="text-sm font-medium text-gray-900 truncate dark:text-white"
-                                            >
+                                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
                                                 Fitness Center
                                             </p>
                                         </div>
                                         <div
-                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
-                                        >
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
                                             +$15
-                                            <span
-                                                class="text-sm font-medium text-gray-900"
-                                            >
-                                                /person/day</span
-                                            >
+                                            <span class="text-sm font-medium text-gray-900">
+                                                /person/day</span>
                                         </div>
                                     </div>
                                 </li>
                                 <li class="py-3 sm:py-4">
                                     <div class="flex items-center space-x-4">
                                         <div class="flex-1 min-w-0">
-                                            <p
-                                                class="text-sm font-medium text-gray-900 truncate dark:text-white"
-                                            >
+                                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
                                                 Parking
                                             </p>
                                         </div>
                                         <div
-                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
-                                        >
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
                                             +$12
-                                            <span
-                                                class="text-sm font-medium text-gray-900"
-                                            >
-                                                /person/day</span
-                                            >
+                                            <span class="text-sm font-medium text-gray-900">
+                                                /person/day</span>
                                         </div>
                                     </div>
                                 </li>
@@ -310,6 +200,7 @@
 <script setup>
 import { Link } from "@inertiajs/vue3";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import FeaturesAndAmenities from '@/Components/RoomComponents/ FeaturesAndAmenities.vue';
 const props = defineProps({
     room: Object,
 });
