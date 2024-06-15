2b02b0f22d381587e4d1a8d170f83552a70ba518
M-erick <muriithierick3758@gmail.com>
2024-06-16 00:15:51 +0300

    submit button

---
+++ resources/js/Components/IssueComponent/SubmitButton.vue
@@ -0,0 +1,10 @@
+<template>
+    <button type="submit" class=" text-white py-2 px-4 rounded mt-3">Submit</button>
+
+</template>
+
+<style scoped>
+button {
+    background-color: #046a5b;
+}
+</style>
