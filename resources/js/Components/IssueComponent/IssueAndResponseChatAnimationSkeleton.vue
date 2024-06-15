257be10a958fbfcffefeeb302d6c320b22f273e9
M-erick <muriithierick3758@gmail.com>
2024-06-15 23:55:43 +0300

    restucture the animation to potray a chat

---
+++ resources/js/Components/IssueComponent/IssueAndResponseChatAnimationSkeleton.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div>
+
+        <AnimatedPlaceholder
+            class="animate-pulse bg-gradient-to-r from-gray-200 to-gray-300  justify-end w-1/4 relative mb-4 mr-2 py-3 px-4  rounded-bl-3xl rounded-tr-xl" />
+        <AnimatedPlaceholder
+            class="animate-pulse bg-gradient-to-r from-gray-200 to-gray-300  justify-end w-1/4 relative mb-4 mr-2 py-3 px-4  rounded-bl-3xl rounded-tr-xl" />
+
+
+
+
+    </div>
+</template>
+
+<script setup>
+import AnimatedPlaceholder from "../AnimatedPlaceholder.vue";
+</script>
