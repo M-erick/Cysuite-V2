ce7d075dd066d1e7405d7453a84dc9c8d2ebe851
M-erick <muriithierick3758@gmail.com>
2024-06-16 00:00:20 +0300

    Chat components respresenting messaging between admin and guest

--- resources/js/Components/IssueComponent/IssueAndResponseChat.vue
+++ resources/js/Components/IssueComponent/IssueAndResponseChat.vue
@@ -1,28 +1,37 @@
 <template>
 
-    <div v-for="response in responses" :key="response.id" :class="{
-        'justify-end':
-            response.user_id === currentUser.id,
-        'justify-start':
-            response.user_id !== currentUser.id,
-    }" class="flex mb-4">
-        <span>@UserID::<span class="font-semibold">{{
-            response.user_id
-                }}</span></span>
-        <div class="relative mb-4 mr-2 py-3 px-4 bg-green-900 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-            {{ response.response_text }}
-            <span class="absolute bottom-0 italic left-0 text-xs text-gray-400 transform translate-y-full">{{
-                formatTimestamp(response.created_at)
-                }}</span>
+    <div>
+        <div v-if="loading">
+            <div v-for="response in responses" :key="response.id" :class="{
+                'justify-end':
+                    response.user_id === currentUser.id,
+                'justify-start':
+                    response.user_id !== currentUser.id,
+            }" class="flex mb-4">
+                <span>@UserID::<span class="font-semibold">{{
+                    response.user_id
+                        }}</span></span>
+                <div
+                    class="relative mb-4 mr-2 py-3 px-4 bg-green-900 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
+                    {{ response.response_text }}
+                    <span class="absolute bottom-0 italic left-0 text-xs text-gray-400 transform translate-y-full">{{
+                        formatTimestamp(response.created_at)
+                    }}</span>
+                </div>
+                <i :class="['fa-solid fa-check text-white py-2 px-4 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl m-3', { 'bg-green-900': !isIssueClosed, 'bg-gray-500': isIssueClosed }]"
+                    @click="markAsResolved(response.id)">
+                </i>
+                <RateAdminResponse :isIssueClosed="isIssueClosed" :responseId="response.id" />
+
+            </div>
+
         </div>
-        <i :class="['fa-solid fa-check text-white py-2 px-4 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl m-3', { 'bg-green-900': !isIssueClosed, 'bg-gray-500': isIssueClosed }]"
-            @click="markAsResolved(response.id)">
-        </i>
-        <RateAdminResponse :isIssueClosed="isIssueClosed" :responseId="response.id" />
+        <div v-else>
+            <IssueAndResponseChatAnimationSkeleton />
 
+
+        </div>
     </div>
-
-
 </template>
 
 <script setup>
@@ -30,16 +39,24 @@ import { usePage } from "@inertiajs/vue3";
 import { ref, computed } from "vue";
 import { useStore } from "vuex";
 import RateAdminResponse from './RateAdminResponse.vue';
+import IssueAndResponseChatAnimationSkeleton from './IssueAndResponseChatAnimationSkeleton.vue';
 
 const store = useStore();
 const { props } = usePage();
 const currentUser = props.auth.user;
+const loading = ref(false);
+let responses = ref([]);
 
 const selectedIssue = computed(() => store.state.issues.selectedIssue);
-const responses = computed(() => store.state.issues.responses);
+
+setTimeout(() => {
+    loading.value = true;
+    responses = computed(() => store.state.issues.responses);
+
+
+}, 2000);
 // check if issue has being resolved
 const isIssueClosed = computed(() => selectedIssue.value && selectedIssue.value.status === 'closed');
-console.log(responses);
 
 const formatTimestamp = (timestamp) => {
     const date = new Date(timestamp);
